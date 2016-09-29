c398ea4768494b95d15199eaee9d612546dd202b
TusharSachde <tushar@wohlig.com>
2016-09-29 16:36:01 +0530

    Success popup added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', ['ngCordova'])
 
-.controller('ImageUploadCtrl', function ($scope, $cordovaCamera) {
+.controller('ImageUploadCtrl', function ($scope, $cordovaCamera, $timeout, $ionicPopup) {
   $scope.openLibrary = function () {
     var libraryOptions = {
       quality: 90,
@@ -21,4 +21,20 @@ angular.module('starter.controllers', ['ngCordova'])
       console.log(err);
     });
   };
+
+  $scope.uploadSuccess = function () {
+    $scope.data = {};
+    var myPopup = $ionicPopup.show({
+      template: '<div class="text-center"><h1 class="icon ion-checkmark-circled balanced"></h1></div>',
+      title: 'Image Uploaded',
+      subTitle: 'Image succesfully uploaded',
+      scope: $scope,
+      buttons: []
+    });
+
+    $timeout(function () {
+      myPopup.close(); //close the popup after 3 seconds for some reason
+    }, 3000);
+  };
+
 });
