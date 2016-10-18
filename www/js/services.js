e3632b43859605c7fb826e19c951228dff96f2b0
gadarohan17 <gadarohan17@gmail.com>
2016-10-18 16:42:50 +0530

    changes

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
-// var adminURL = "http://146.148.42.15:83/api/";
-var adminURL = "http://192.168.0.104:1337/api/";
+var adminURL = "http://146.148.42.15:83/api/";
+// var adminURL = "http://192.168.0.104:1337/api/";
 var uploadurl = adminURL + "upload/";
 angular.module('starter.services', [])
 
